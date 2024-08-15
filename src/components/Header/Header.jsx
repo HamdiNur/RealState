43916c58800803f7b56b81e8a9f7ff03e04853c8
HamdiNur <hamdoonur26@gmail.com>
2024-08-15 20:15:03 +0300

    creating components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+     <section className="h-wrapper">
+        <div className="h-container">
+            <img src="./logo.png" alt="" width={100} />
+
+            <div className="h-menu">
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+                <a href=""></a>
+            </div>
+        </div>
+     </section>
+  );
+}
+
+export default Header;
