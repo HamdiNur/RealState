58876ed2ccdf007a5268e799e698535d8a052b1b
HamdiNur <hamdoonur26@gmail.com>
2024-08-22 18:17:38 +0300

    residence  section finished

--- src/components/Residencies/Residencies.jsx
+++ src/components/Residencies/Residencies.jsx
@@ -4,6 +4,7 @@ import {Swiper,SwiperSlide,useSwiper} from 'swiper/react'
 import "swiper/css"
 import "./Residencies.css"
 import data from "../../utils/slider.json"
+import { sliderSettings } from '../../utils/common';
 const Residencies = () => {
   return (
      <section className="r-wrapper">
@@ -12,7 +13,8 @@ const Residencies = () => {
                 <span className='orangeText'>Best Choices</span>
                 <span className='primaryText'>Popular Residencies</span>
             </div>
-            <Swiper>
+            <Swiper {...sliderSettings}>
+                <SliderButtons/>
                 {
                     data.map((card,i)=>(
                         <SwiperSlide key={i}>
@@ -36,3 +38,13 @@ const Residencies = () => {
 }
 
 export default Residencies;
+const SliderButtons= () =>{
+    const swiper=useSwiper();
+    return(
+        <div className=" flexCenter r-buttons">
+            <button onClick={()=>swiper.slidePrev()}>
+                &lt;</button>
+                <button  onClick={()=>swiper.slideNext()}>&gt;</button>
+        </div>
+    )
+}
